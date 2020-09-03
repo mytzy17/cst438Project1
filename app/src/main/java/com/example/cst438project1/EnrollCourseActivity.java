8bea0e6c2a207bf7040fa9c7953c30fc9abb0d3d
Mytzy Escalante <mytzyescalante17@gmail.com>
2020-09-02 17:56:27 -0700

    Created activities

---
+++ app/src/main/java/com/example/cst438project1/EnrollCourseActivity.java
@@ -0,0 +1,14 @@
+package com.example.cst438project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnrollCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enroll_course);
+    }
+}
