fe7ef012e6980732aece79fa3c53b07ee46718bd
Mytzy Escalante <mytzyescalante17@gmail.com>
2020-09-07 01:18:45 -0700

    Created home, login, register activity

--- app/src/main/java/com/example/cst438project1/DB/AppDatabase.java
+++ app/src/main/java/com/example/cst438project1/DB/AppDatabase.java
@@ -8,10 +8,23 @@ import androidx.room.RoomDatabase;
 
 @Database (entities = {AccountLog.class}, version=1)
 //Can put a type converter here IF we need one
+
 public abstract class AppDatabase extends RoomDatabase {
     public static final String databaseName = "database-account-log";
 
+    //this is the AccountLog DB
     public static final String ACCOUNTLOG_TABLE = "accountlog_table";
+    //Probs we will also need, added them if we need them
+    public static final String COURSE_TABLE ="course table";
 
+    public static final String GRADE_TABLE ="grade table";
+
+    public static final String ASSIGNMENT_TABLE="assignment table";
+
+    public static final String GRADE_CATEGORY_TABLE="grade category table";
+
+    public static final String ENROLLMENT_TABLE="enrollment table";
+
+    //Functions
     public abstract AccountDAO getAccountDAO();
 }
--- app/src/main/java/com/example/cst438project1/MainActivity.java
+++ app/src/main/java/com/example/cst438project1/MainActivity.java
@@ -1,10 +1,13 @@
+//HomePage Activity: User will be able either register, login, view logs, or exit App
 package com.example.cst438project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-// Test commit: Christopher v2
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //User will be taken to the CreateAccount Activity
+        Button createAccountButton = findViewById(R.id.createAccount);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Will take us to the create account activity now
+                Log.d("MainActivity", "CreateAccount Activity was called");
+
+                //Create our intent
+                Intent i = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        //User will be taken to the Login Activity
+        Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Will take us to the Login Activity
+                Log.d("LoginActivity", "Login Activity was called");
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        //User will be taken to the View
+        Button viewLog = findViewById(R.id.viewLogs);
+        viewLog.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Will take us to the View Log Activity
+                Log.d("ViewLogActivity", "ViewLog Activity was called");
+                Intent i = new Intent(MainActivity.this, ViewProfileActivity.class);
+                startActivity(i);
+            }
+        }));
+
+        Button exitButton = findViewById(R.id.exitApp);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ExitApp", "Exit App call was called");
+                //this will make it close anywhere where the user is
+                finish();
+            }
+        });
+
     }
+
+
+
+
+
 }
