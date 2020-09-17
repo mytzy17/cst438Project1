adb72c1a2786e321dec7dc26781bbb2655b6e3cc
Mytzy Escalante <mytzyescalante17@gmail.com>
2020-09-16 22:12:32 -0700

    EnrollActivity was edited

--- app/src/main/java/com/example/cst438project1/EnrollCourseActivity.java
+++ app/src/main/java/com/example/cst438project1/EnrollCourseActivity.java
@@ -1,19 +1,28 @@
 package com.example.cst438project1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.cst438project1.DB.AccountDAO;
+import com.example.cst438project1.DB.AccountLog;
 import com.example.cst438project1.DB.AppDatabase;
 import com.example.cst438project1.DB.CourseDAO;
 import com.example.cst438project1.DB.CourseDatabase;
 import com.example.cst438project1.DB.EnrollDAO;
 import com.example.cst438project1.DB.EnrollDatabase;
+import com.example.cst438project1.DB.EnrollLog;
 
 public class EnrollCourseActivity extends AppCompatActivity {
 
+    Button enrollButton;
+    EditText courseName;
     private AccountDAO accountLogDAO;
     private AppDatabase db;
 
@@ -38,7 +47,26 @@ public class EnrollCourseActivity extends AppCompatActivity {
 
         enrollDAO = db2.getEnrollDAO();
 
-        //Hello just checking here
+        enrollButton = findViewById(R.id.enrollButton);
+        courseName = findViewById(R.id.courseName);
+
+        enrollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String course = courseName.getText().toString();
+
+                if(course.equals("")){
+                    AlertDialog.Builder b = new AlertDialog.Builder(EnrollCourseActivity.this);
+                    b.setMessage("Incomplete Field");
+                    b.setPositiveButton("DISMISS", null);
+
+                    b.show();
+                    return;
+                }
+
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/example/cst438project1/MenuActivity.java
+++ app/src/main/java/com/example/cst438project1/MenuActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
+    Button enrollButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +20,13 @@ public class MenuActivity extends AppCompatActivity {
 
         Button profileButton = findViewById(R.id.profileButton);
         Button addCourseButton = findViewById(R.id.addCourse);
+        enrollButton = findViewById(R.id.enrollButton);
+
         final String userName = getIntent().getStringExtra("username");
         final String passWord = getIntent().getStringExtra("password");
         final String fName = getIntent().getStringExtra("fName");
         final String lName = getIntent().getStringExtra("lName");
+
 
         addCourseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,10 +34,15 @@ public class MenuActivity extends AppCompatActivity {
                 //Create our intent
                 Intent i = new Intent(MenuActivity.this, AddCourseActivity.class);
 
-//                i.putExtra("username", userName);
-//                i.putExtra("fName", fName);
-//                i.putExtra("lName", lName);
-//                i.putExtra("password", passWord);
+                startActivity(i);
+            }
+        });
+
+        enrollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create our intent
+                Intent i = new Intent(MenuActivity.this, EnrollCourseActivity.class);
                 startActivity(i);
             }
         });
